7552034f07a85a52e8806f0c1b3bf85ea949c962
Sunil S R <sunilsrdsa@gmail.com>
2025-06-05 12:28:12 +0530

    Cors annotation

--- restapi/src/main/java/in/sunilsr/restapi/controller/ExpenseController.java
+++ restapi/src/main/java/in/sunilsr/restapi/controller/ExpenseController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*") // allow all request
 public class ExpenseController {
 
     private static final Logger log = LoggerFactory.getLogger(ExpenseController.class);
